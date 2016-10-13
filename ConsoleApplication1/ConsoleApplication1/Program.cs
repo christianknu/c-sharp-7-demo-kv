8297cd88a49a4cfde70d52442451e7b0cc25eef4
Christian Knudsen <christian.knudsen@kraftvaerk.com>
2016-10-13 21:00:32 +0200

    Initial commit of code...

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using static System.Console;
+
+namespace CSharp7DemoKV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *  General notes: 
+             *  Tuple type -> System.ValueTuple
+             *  You are not allowed to change the order of Item1, Item2 etc.
+             */
+
+            WriteLine("---- Step 1 ----");
+            step1.Run();
+            WriteLine("\n");
+
+            WriteLine("---- Step 2 ----");
+            step2.Run();
+            WriteLine("\n");
+
+            WriteLine("---- Step 3 ----");
+            step3.Run();
+            WriteLine("\n");
+
+            WriteLine("---- Step 4 ----");
+            step4.Run();
+            WriteLine("\n");
+
+            WriteLine("---- Step 5 ----");
+            step5.Run();
+            WriteLine("\n");
+
+            WriteLine("---- Step 6 ----");
+            step6.Run();
+            WriteLine("\n");
+        }
+    }
+
+    static class step1
+    {
+        public static void Run()
+        {
+            // 1. Binary literals mode in C# 7
+            int[] numbers = { 0b1, 0b10, 0b100, 0b1000, 0b1_0000, 0b10_0000 };
+            
+            // 2. Deep analysis, count the numbers and add them up
+            var t = Tally(numbers);
+            WriteLine($"sum: {t.Item1}, count: {t.Item2}");
+        }
+        
+        private static (int, int) Tally(Int32[] numbers)
+        {
+            return (0, 0);
+        }
+    }
+
+    static class step2
+    {
+        public static void Run()
+        {
+            int[] numbers = { 0b1, 0b10, 0b100, 0b1000, 0b1_0000, 0b10_0000 };
+            var t = Tally(numbers);
+            WriteLine($"sum: {t.Item1}, count: {t.Item2}");
+        }
+        
+        private static (int, int) Tally(Int32[] numbers)
+        {
+            return (s:0, C:0);
+        }
+    }
+
+    static class step3
+    {
+        public static void Run()
+        {
+            int[] numbers = { 0b1, 0b10, 0b100, 0b1000, 0b1_0000, 0b10_0000 };
+            var t = Tally(numbers);
+            WriteLine($"sum: {t.Item1}, count: {t.Item2}");
+        }
+
+        private static (int, int) Tally(Int32[] numbers)
+        {
+            return (s: 0, C: 0);
+        }
+    }
+
+    static class step4
+    {
+        public static void Run()
+        {
+            int[] numbers = { 0b1, 0b10, 0b100, 0b1000, 0b1_0000, 0b10_0000 };
+            var t = Tally(numbers);
+            WriteLine($"sum: {t.sum}, count: {t.count}");
+        }
+
+        private static (int sum, int count) Tally(Int32[] numbers)
+        {
+            var r = (s: 0, c: 0);
+            return r;
+        }
+    }
+
+    static class step5
+    {
+        public static void Run()
+        {
+            int[] numbers = { 0b1, 0b10, 0b100, 0b1000, 0b1_0000, 0b10_0000 };
+
+            // Deconstrution of tuples
+            var (sum, count) = Tally(numbers);
+
+            // Using variables from deconstructed tuple
+            WriteLine($"sum: {sum}, count: {count}");
+        }
+
+        private static (int sum, int count) Tally(Int32[] numbers)
+        {
+            var r = (s: 0, c: 0);
+            foreach (var v in numbers)
+            {
+                //r = (r.s + v, r.c + 1);
+                // Because s and c are just public fields
+                r.s += v; r.c++;
+            }
+            return r;
+        }
+    }
+
+    static class step6
+    {
+        public static void Run()
+        {
+            int[] numbers = { 0b1, 0b10, 0b100, 0b1000, 0b1_0000, 0b10_0000 };
+
+            // Deconstrution of tuples
+            var (sum, count) = Tally(numbers);
+
+            // Using variables from deconstructed tuple
+            WriteLine($"sum: {sum}, count: {count}");
+        }
+
+        private static (int sum, int count) Tally(Int32[] numbers)
+        {
+            var r = (s: 0, c: 0);
+            foreach (var v in numbers)
+            {
+                // Local method use
+                Add(v, 1);
+            }
+            return r;
+
+            // Local method
+            void Add(int s, int c) { r = (r.s + s, r.c + c); }
+        }
+    }
+
+    static class step7
+    {
+        public static void Run()
+        {
+            // Making number an array of objects
+            object[] numbers = { 0b1, 0b10, new object[] { 0b100, 0b1000, 0b1_0000 }, null, 0b10_0000 };
+
+            var (sum, count) = Tally(numbers);
+            WriteLine($"sum: {sum}, count: {count}");
+        }
+
+        private static (int sum, int count) Tally(object[] numbers)
+        {
+            var r = (s: 0, c: 0);
+            foreach(var v in numbers)
+            {
+                // Patterns: Checking type and assigning value to variable
+                if (v is int i)
+                {
+                    Add(i, 1);
+                }
+            }
+            return r;
+            void Add(int s, int c) { r = (r.s + s, r.c + c); }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-c-sharp-7-demo-kv
+# C#7 Demo at Dev Meeting at Kraftvaerk Copenhagen 14.10.2016
+
+## Requirements
+To run the small demo code, you need Microsoft Visual Studio 15 Preview 5 and up
